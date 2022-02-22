e83f0d9927ebc0a8f8758a81b11d242c9f5c7f2f
lws803 <lws803@gmail.com>
2022-02-22 23:09:00 +0000

    added test for successful login

--- src/pages/login.test.tsx
+++ src/pages/login.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, act } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import Login from './login';
+import * as api from './login/api';
 
 describe(Login, () => {
   const path = '/?platformId=1&callback=https://www.example.com';
@@ -26,5 +27,39 @@ describe(Login, () => {
     expect(
       await findByText('PlatformId and callback is not present.'),
     ).toBeInTheDocument();
+  });
+
+  it.only('logs in successfully', async () => {
+    const windowOpen = jest.spyOn(window, 'open');
+    const loginWithPlatform = jest
+      .spyOn(api, 'loginWithPlatform')
+      .mockResolvedValue({ data: { code: 'CODE' }, error: null });
+
+    const { getByLabelText, getByText, findByText } = render(
+      <MemoryRouter initialEntries={[path]}>
+        <Login />
+      </MemoryRouter>,
+    );
+
+    act(() => {
+      fireEvent.change(getByLabelText('Email input'), {
+        target: { value: 'TEST@EMAIL.COM' },
+      });
+      fireEvent.change(getByLabelText('Password input'), {
+        target: { value: 'PASSWORD' },
+      });
+    });
+    fireEvent.click(getByText('Login'));
+
+    expect(await findByText('Login successful')).toBeInTheDocument();
+
+    expect(loginWithPlatform).toHaveBeenCalledWith({
+      callback: 'https://www.example.com',
+      platformId: 1,
+      values: {
+        email: 'TEST@EMAIL.COM',
+        password: 'PASSWORD',
+      },
+    });
   });
 });
