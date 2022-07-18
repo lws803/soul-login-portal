f3b2b3881ed216466d5f2014dfa0f106ed79b23c
lws803 <lws803@gmail.com>
2022-07-18 16:04:02 +0800

    refactored request email verification form

--- src/Pages/RequestEmailVerification/Form.tsx
+++ src/Pages/RequestEmailVerification/Form.tsx
@@ -1,4 +1,4 @@
-import { useFormik } from 'formik';
+import { Formik, Field, Form as FormikForm, FieldProps } from 'formik';
 import * as Yup from 'yup';
 import {
   FormControl,
@@ -12,60 +12,60 @@ import {
 import { sendEmailConfirmation } from './api';
 
 export default function Form({ setErrors, setIsSuccess }: Props) {
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-    },
-    onSubmit: async (values) => {
-      setErrors([]);
-      const { error } = await sendEmailConfirmation(values);
-
-      if (error) {
-        setErrors([error.message]);
-      } else {
-        setIsSuccess(true);
-      }
-    },
-    validationSchema: Yup.object({
-      email: Yup.string().email().required(),
-    }),
-    validateOnChange: false,
-  });
-
   return (
-    <form onSubmit={(e) => formik.handleSubmit(e as any)}>
-      <FormControl
-        isInvalid={!!formik.errors.email && formik.touched.email}
-        marginTop={8}
-      >
-        <FormLabel htmlFor="email">Email</FormLabel>
-        <Input
-          id="email"
-          name="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-          aria-label="Email input"
-          variant="filled"
-          placeholder="Enter your email"
-        />
-        {formik.errors.email && (
-          <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-        )}
-      </FormControl>
-      <HStack mt={8}>
-        <Button
-          type="submit"
-          isLoading={formik.isSubmitting}
-          loadingText="Sending verification email"
-          width="100%"
-          bg="soul.pink.light"
-          _hover={{ bg: 'soul.pink.dark' }}
-          _active={{ bg: 'soul.pink.dark' }}
-        >
-          Request email verification
-        </Button>
-      </HStack>
-    </form>
+    <Formik
+      initialValues={{ email: '' }}
+      onSubmit={async (values) => {
+        setErrors([]);
+        const { error } = await sendEmailConfirmation(values);
+        if (error) {
+          setErrors([error.message]);
+        } else {
+          setIsSuccess(true);
+        }
+      }}
+      validationSchema={Yup.object({
+        email: Yup.string().email().required(),
+      })}
+      validateOnChange={false}
+    >
+      {({ isSubmitting }) => (
+        <FormikForm>
+          <Field name="email">
+            {({ field, form }: FieldProps<string>) => (
+              <FormControl
+                isInvalid={!!form.errors.email && !!form.touched.email}
+                marginTop={8}
+              >
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input
+                  {...field}
+                  aria-label="Email input"
+                  variant="filled"
+                  placeholder="Enter your email"
+                />
+                {form.errors.email && (
+                  <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                )}
+              </FormControl>
+            )}
+          </Field>
+          <HStack mt={8}>
+            <Button
+              type="submit"
+              isLoading={isSubmitting}
+              loadingText="Sending verification email"
+              width="100%"
+              bg="soul.pink.light"
+              _hover={{ bg: 'soul.pink.dark' }}
+              _active={{ bg: 'soul.pink.dark' }}
+            >
+              Request email verification
+            </Button>
+          </HStack>
+        </FormikForm>
+      )}
+    </Formik>
   );
 }
 
