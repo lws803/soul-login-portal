910a466d8c777af63d9931cf30c2038c5d099cbb
Wilson Ler <lws803@gmail.com>
2022-07-31 20:55:47 +0800

    Add discord invite url in footer (#49)
    * add discord invite url in footer
    
    * updated snapshots

--- src/components/Page/Footer.tsx
+++ src/components/Page/Footer.tsx
@@ -20,6 +20,13 @@ export default function Footer() {
             Soul&apos;s organization Github
           </Link>
           <Link
+            href="https://discord.gg/kuGNRembKZ"
+            target="_blank"
+            whiteSpace="nowrap"
+          >
+            Discord server
+          </Link>
+          <Link
             href="mailto:lws803@gmail.com"
             target="_blank"
             whiteSpace="nowrap"
